4c39709e729b5899d65d471cefa7a2793ef8f6ac
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2025-01-24 02:27:48 +0200

    התאמת עוצמת מוזיקת רקע

--- scripts/build-shorts-multi-languages.py
+++ scripts/build-shorts-multi-languages.py
@@ -848,7 +848,7 @@ class VideoAssemblerShorts:
                 final_clip = concatenate_videoclips(clips, method="compose")
 
                 if os.path.exists(BACKGROUND_MUSIC_PATH):
-                    background_music = AudioFileClip(BACKGROUND_MUSIC_PATH).volumex(0.03)
+                    background_music = AudioFileClip(BACKGROUND_MUSIC_PATH).volumex(0.01)
                     background_music = audio_loop(background_music, duration=final_clip.duration)
                     final_audio = CompositeAudioClip([final_clip.audio, background_music])
                     final_clip = final_clip.set_audio(final_audio)
