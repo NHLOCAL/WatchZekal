8985f9dc27503c4e2390b033cafd7ae7eaddf565
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2025-08-24 22:51:11 +0300

    שיפור: התעלמות מקבצים עבור יצירת ריפומיקס

--- song_subtitler/run_repomix.bat
+++ song_subtitler/run_repomix.bat
@@ -1,5 +1,5 @@
 @echo off
 
-npx repomix --style markdown --remove-comments
+npx repomix --style markdown --remove-comments -i "output/,srt_files/,data/,**/system_instructions.yaml,**/song_list.json"
 
 pause
