7e418f23b79c0e16e7eb138d1d46f18a4071ece4
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2025-08-24 22:52:04 +0300

    התעלמות נוספת עבור ריפומיקס

--- song_subtitler/run_repomix.bat
+++ song_subtitler/run_repomix.bat
@@ -1,5 +1,5 @@
 @echo off
 
-npx repomix --style markdown --remove-comments -i "output/,srt_files/,data/,**/system_instructions.yaml,**/song_list.json"
+npx repomix --style markdown --remove-comments -i "output/,srt_files/,data/,**/system_instructions.yaml,**/song_list.json,gui.py,runner.py"
 
 pause
